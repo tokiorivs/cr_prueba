69bb5aedeb1c9728703a074142f668418273a4ce
cesar <cesaroumeres@gmail.com>
2026-01-14 17:01:12 -0500

    se mejoro el formato para los numeros electronicos y los numeros consecutivos en la parte inferior

--- custom_addons/l10n_cr_extended/models/l10n_cr_custom.py
+++ custom_addons/l10n_cr_extended/models/l10n_cr_custom.py
@@ -4,10 +4,14 @@ from odoo import models, fields, api
 class L10nCrCustom(models.Model):
     _inherit = 'account.move'
 
-    electronic_number = fields.Char(string="Electronic Number", readonly=False, default="0012219292")
-    electronic_consecutive = fields.Char(string="Electronic Consecutive", readonly=False, default="2033939")
+    def _get_electronic_number(self):
+        return "0" * 50
 
+    def _get_electronic_consecutive(self):
+        return "0" * 20
 
+    electronic_number = fields.Char(string="Electronic Number", readonly=False, default=_get_electronic_number)
+    electronic_consecutive = fields.Char(string="Electronic Consecutive", readonly=False, default=_get_electronic_consecutive)
 
 
 
