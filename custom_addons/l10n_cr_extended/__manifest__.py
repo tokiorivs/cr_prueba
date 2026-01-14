b68f6d6a8646e698c7eb991d67c4ed4b7e969854
cesar <cesaroumeres@gmail.com>
2026-01-14 08:14:38 -0500

    se agrego la dependencia l10n_latam_base

--- custom_addons/l10n_cr_extended/__manifest__.py
+++ custom_addons/l10n_cr_extended/__manifest__.py
@@ -8,7 +8,7 @@ Extended functionalities for l10n_cr.
 This module inherits from l10n_cr to allow custom modifications for Costa Rica localization.
     """,
     'author': 'User',
-    'depends': ['base', 'l10n_cr', 'account', 'l10n_latam_invoice_document'],
+    'depends': ['base', 'l10n_cr', 'account', 'l10n_latam_invoice_document', 'l10n_latam_base'],
     'data': [
         'views/account_move_views.xml',
         'views/report_invoice.xml',
